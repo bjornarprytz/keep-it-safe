fa8bc1b959f51e7d19c5a36055db166a6f262e76
Bjørnar Prytz <bjornarprytz@gmail.com>
2024-12-01 02:08:24 +0100

    fix a bug

--- src/main.gd
+++ src/main.gd
@@ -8,6 +8,8 @@ const scary_track = preload("res://assets/audio/KeepitsafeSCARYAMBIENT.wav")
 
 @onready var text:DialogueResource  = preload("res://dialogue/test.dialogue")
 
+var dialog_started: bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Events.gameOver.connect(on_game_over)
@@ -23,11 +25,14 @@ func _on_exit_body_entered(body: Node2D) -> void:
 	if body.owner is Player:
 		get_tree().call_deferred("reload_current_scene")
 
-
 func _on_troll_area_entered(area: Area2D) -> void:
+	if dialog_started:
+		return
+	
 	if area.owner is Player:
 		$UI.show()
 		dialogue.start(text, "start")
+		dialog_started = true
 
 func on_game_over(success: bool):
 	get_tree().change_scene_to_file("res://outro.tscn")
--- src/troll.gd
+++ src/troll.gd
@@ -3,9 +3,14 @@ extends Area2D
 
 @onready var sprite: Sprite2D = %Sprite
 
+var is_revealed: bool
+
 
 func _on_area_entered(area: Area2D) -> void:
+	if is_revealed:
+		return
 	if area.owner is Player:
+		is_revealed = true
 		area.owner.stopped = true
 		var tween = create_tween().set_ease(Tween.EASE_IN)
 		sprite.modulate.a = 0.0
